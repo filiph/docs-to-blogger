0021e5fc4a39f3524461d9500f98966235b3e170
Filip Hracek <filiph@google.com>
2012-10-03 20:19:50 +0200

    initial commit

---
+++ docsToBlogger.dart
@@ -0,0 +1,22 @@
+#import('dart:html');
+
+#import('docsToBloggerConverter.dart');
+
+void main() {
+  DocsToBloggerConverter converter = new DocsToBloggerConverter();
+
+  ButtonElement convertBtn = document.query("#convertBtn");
+  TextAreaElement inputTextEl = document.query("#inputText");
+  TextAreaElement strongIdEl = document.query("#strongId");
+  TextAreaElement outputTextEl = document.query("#outputText");
+  DivElement outputTextDivEl = document.query("#outputTextDiv");
+
+  convertBtn.on.click.add((e) {
+    outputTextEl.value = outputTextDivEl.innerHTML
+        = converter.convert(
+            inputTextEl.value,
+            strongId:strongIdEl.value
+    );
+  });
+
+}
---
+++ docsToBloggerConverter.dart
@@ -0,0 +1,62 @@
+#library('docsToBlogger');
+
+
+class DocsToBloggerConverter {
+  static final RegExp _header = const RegExp(@"<html.*</head><body.*?>");
+  static final RegExp _footer = const RegExp(@"</body></html>");
+  static final RegExp _spanBegin = const RegExp(@"<span.*?>");
+  static final RegExp _spanEnd = const RegExp(@"</span>");
+  static final RegExp _spanClassC = const RegExp(@'<span class="(c.+?)">(.+?)</span>', multiLine:true);
+  static final RegExp _aname = const RegExp("<a name=\".*?\"></a>");
+  static final RegExp _cssClass = const RegExp(' class=".*?"');
+  static final RegExp _pBegin = const RegExp(@"<p.*?>");
+  static final RegExp _lineEnd = const RegExp(@"(</p>)|(<br/?>)");
+
+  DocsToBloggerConverter() {
+  }
+
+  String convert(String input, [String strongId="c3"]) {
+    StringBuffer strBuf = new StringBuffer();
+    int index = 0;
+
+    // find <span class="c2">...</span> and convert to <strong>...</strong>
+    Match m = _spanClassC.firstMatch(input);
+    while (m != null) {
+      print("index is $index, match found from ${index+m.start()} to ${index+m.end()}");
+      strBuf.add(input.substring(index,index + m.start()));
+
+      int groupCount = m.groupCount();
+      print("Group Count = $groupCount");
+      String classesStr = m.group(1);
+      Set<String> classes = new Set<String>.from(classesStr.split(" "));
+
+      if (classes.contains(strongId.trim())) {
+        strBuf.add("<strong>");
+        strBuf.add(m.group(groupCount));
+        //strBuf.add("${groupCount -1}");
+        strBuf.add("</strong>");
+      } else {
+        strBuf.add(m.group(groupCount));
+      }
+      index = index + m.end();
+      m = _spanClassC.firstMatch(input.substring(index));
+    }
+
+    // add the rest
+    strBuf.add(input.substring(index));
+
+    input = strBuf.toString();
+
+    String output = input
+        .replaceAll(_header, "")
+        .replaceAll(_footer, "")
+        .replaceAll(_spanBegin, "")
+        .replaceAll(_spanEnd, "")
+        .replaceAll(_aname, "")
+        .replaceAll(_cssClass, "")
+        .replaceAll(_pBegin, "")
+        .replaceAll(_lineEnd, "\n");
+
+    return output;
+  }
+}
