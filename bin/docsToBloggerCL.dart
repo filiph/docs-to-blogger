9a8ec3a59cdeb56b022c8454b1a845466a8f9470
Filip Hracek <filip.hracek@gmail.com>
2016-11-18 17:46:33 -0800

    Remove stub of Angular

--- bin/docsToBloggerCL.dart
+++ bin/docsToBloggerCL.dart
@@ -1,16 +1,16 @@
-#import('dart:io');
-#import('docsToBloggerConverter.dart');
+import 'dart:io';
+import 'package:docs_to_blogger/docsToBloggerConverter.dart';
 
-#import('package:args/args.dart');
+import 'package:args/args.dart';
 
-void main() {
+void main(List<String> args) {
   var parser = new ArgParser();
 
   parser.addOption("strong", abbr:"s", defaultsTo:"c1");
   parser.addOption("em", abbr:"e", defaultsTo:"c2");
   parser.addFlag("ptags", abbr:"p", defaultsTo:false);
 
-  var results = parser.parse(new Options().arguments);
+  var results = parser.parse(args);
   if (results.rest.length < 1) {
     print(parser.getUsage());
     return;
@@ -19,7 +19,7 @@ void main() {
   
   new File(filename).open().then((RandomAccessFile f) {
     List<int> buffer = new List<int>(f.lengthSync());
-    f.readListSync(buffer, 0, f.lengthSync());
+    f.readIntoSync(buffer, 0, f.lengthSync());
     
     String input = new String.fromCharCodes(buffer);
     
--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,43 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+import 'dart:html';
 
-//import 'package:angular2/platform/browser.dart';
-//
-//import 'package:update/app_component.dart';
-//
-//main() {
-//  bootstrap(AppComponent);
-//}
+import 'package:docs_to_blogger/docsToBloggerConverter.dart';
+
+void main() {
+  DocsToBloggerConverter converter = new DocsToBloggerConverter();
+
+  ButtonElement convertBtn = document.query("#convertBtn");
+  TextAreaElement inputTextEl = document.query("#inputText");
+  TextAreaElement strongIdEl = document.query("#strongId");
+  TextAreaElement emIdEl = document.query("#emId");
+  InputElement removePTagsEl = document.query("#removePTags");
+  TextAreaElement outputTextEl = document.query("#outputText");
+  DivElement outputTextDivEl = document.query("#outputTextDiv");
+  
+  convertBtn.onClick.listen((e) {
+    String convertedText = converter.convert(
+            inputTextEl.value,
+            strongId:strongIdEl.value,
+            emId:emIdEl.value,
+            removePTags:(removePTagsEl.checked)
+    );
+    
+    outputTextEl.value = convertedText;
+    
+    if (removePTagsEl.checked) {
+      outputTextDivEl.innerHtml = convertedText.replaceAll("\n", "<br/>");
+    } else {
+      outputTextDivEl.innerHtml = convertedText;
+    }
+  });
+
+  inputTextEl.onDrop.listen((MouseEvent e) {
+    File inFile = e.dataTransfer.files[0];
+    FileReader reader = new FileReader();
+    reader.readAsText(inFile.slice());
+    reader.onLoadEnd.listen((_) {
+      inputTextEl.value = reader.result;
+    });
+    e.preventDefault();
+  });
+
+}
